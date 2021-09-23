ca34e4ccf15fdc8ad11c96fbe9d3f7334136c583
Esam <lands.of.48@gmail.com>
2021-09-23 11:58:43 +0300

    First commit

--- src/index.js
+++ src/index.js
@@ -1,17 +1,71 @@
-import React from 'react';
+import React,{useState,useEffect} from 'react';
 import ReactDOM from 'react-dom';
+import axios from 'axios';
 import './index.css';
-import App from './App';
-import reportWebVitals from './reportWebVitals';
+import '../node_modules/bootstrap/dist/css/bootstrap.min.css';
 
-ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
-);
 
-// If you want to start measuring performance in your app, pass a function
-// to log results (for example: reportWebVitals(console.log))
-// or send to an analytics endpoint. Learn more: https://bit.ly/CRA-vitals
-reportWebVitals();
+const baseURL = "http://api.quotable.io/random";
+
+
+
+export default function App(){
+    const [quote,setRandomQuote] = useState('');
+    const [randomColor,setRandomColor] = useState({
+        color:'#'+Math.floor(Math.random()*16777215).toString(16),
+    });
+
+    const update = () => {
+        axios.get(baseURL)
+        .then((res)=>{
+            setRandomQuote(res.data);
+        });
+        setRandomColor({
+            color:'#'+Math.floor(Math.random()*16777215).toString(16),
+        });
+        
+    }
+    
+
+    useEffect(update,[]);
+    
+    return(
+        <div>
+            <QuoteCard 
+            quote={quote.content}
+            author={quote.author}
+            handleClick={update}
+            textStyles={randomColor}
+            />
+        </div>
+    )
+}
+
+function QuoteCard(props){
+    return(
+        <div id="quoteCard" className="card shadow p-3">
+            <div className="text-center quoteText">
+                <h3 className="bold lead" style={props.textStyles}>{props.quote}</h3>
+            </div>
+            <div className="quoteAuthor">
+                <p className="float-end" style={props.textStyles}>- {props.author}</p>
+            </div>
+            <div className="buttons d-flex justify-content-between">
+                <div className="social-buttons d-flex justify-content-between">
+                    <button className="btn btn-secondary m-1">
+                    <i className="fab fa-twitter"></i>
+                    </button>
+                    <button className="btn btn-secondary m-1">
+                    <i className="fab fa-instagram"></i>
+                    </button>
+                </div>
+                <div>
+                    <button className="btn btn-secondary" onClick={props.handleClick}>Generate</button>
+                </div>
+            </div>
+        </div>
+    );
+}
+ 
+
+ReactDOM.render(<App/>,document.getElementById('root'))
